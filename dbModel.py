ef7ead37b0b63b800010d0c12f84a4d71e05eec2
kkiiddss <34616443+kkiiddss@users.noreply.github.com>
2017-12-17 23:09:08 +0800

    Update dbModel.py

--- dbModel.py
+++ dbModel.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
-DB_connect = 'postgresql+psycopg2://postgres:PASSWORD@localhost/database_name'
+DB_connect = 'postgres://woruvugwifnphc:d7d6da2c241d65e87abd324ecfa077057bd42a8a919a79fc80b53fbb6e657aab@ec2-184-72-228-128.compute-1.amazonaws.com:5432/del6ucpoe5ftf2e'
 
 
 class Images(Base):
