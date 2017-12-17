190f7446a63db87b0765fe1f9319649239424678
kkiiddss <34616443+kkiiddss@users.noreply.github.com>
2017-12-17 22:57:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
+import time
 import requests
 import re
 import random
 import configparser
 from bs4 import BeautifulSoup
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from flask import Flask, request, abort
+from sqlalchemy import create_engine
+
+from sqlalchemy.orm import sessionmaker
+
+import schedule
+
+import download_beauty
+
+from dbModel import Images, DB_connect
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+rs = requests.session()
 #from imgurpython import ImgurClient
 
 from linebot import (
@@ -98,6 +112,25 @@ def get_page_number(content):
     page_number = content[start_index + 5: end_index]
     return int(page_number) + 1
 
+def over18(board):
+
+    res = rs.get('https://www.ptt.cc/bbs/{}/index.html'.format(board), verify=False)
+
+    # 先檢查網址是否包含'over18'字串 ,如有則為18禁網站
+
+    if 'over18' in res.url:
+
+        print("18禁網頁")
+
+        load = {
+            'from': '/bbs/{}/index.html'.format(board),
+            'yes': 'yes'
+        }
+
+        res = rs.post('https://www.ptt.cc/ask/over18', verify=False, data=load)
+
+    return BeautifulSoup(res.text, 'html.parser')
+
 
 def craw_page(res, push_rate):
     soup_ = BeautifulSoup(res.text, 'html.parser')
@@ -128,6 +161,161 @@ def craw_page(res, push_rate):
             print('本文已被刪除', e)
     return article_seq
 
+def write_db(images, session):
+
+    for image in images:
+
+        is_exist = session.query(Images).filter(Images.Url == image).first()
+
+        if not is_exist:
+
+            data = Images(Url=image)
+
+            session.add(data)
+
+    session.commit()
+
+
+
+
+
+def connect_db(db_string):
+
+    engine = create_engine(db_string)
+
+    db_session = sessionmaker(bind=engine)
+
+    session = db_session()
+
+    return engine, session
+
+
+
+
+
+def main(crawler_pages=2):
+
+    engine, session = connect_db(DB_connect)
+
+    # python beauty_spider2.py [版名]  [爬幾頁] [推文多少以上]
+
+    board, page_term, push_rate = 'beauty', crawler_pages, 10
+
+    start_time = time.time()
+
+    soup = over18(board)
+
+    all_page_url = soup.select('.btn.wide')[1]['href']
+
+    start_page = get_page_number(all_page_url)
+
+
+
+    print("Analytical download page...")
+
+    index_list = []
+
+    article_list = []
+
+    for page in range(start_page, start_page - page_term, -1):
+
+        page_url = 'https://www.ptt.cc/bbs/{}/index{}.html'.format(board, page)
+
+        index_list.append(page_url)
+
+
+
+    # 抓取 文章標題 網址 推文數
+
+    while index_list:
+
+        index = index_list.pop(0)
+
+        res = rs.get(index, verify=False)
+
+        # 如網頁忙線中,則先將網頁加入 index_list 並休息1秒後再連接
+
+        if res.status_code != 200:
+
+            index_list.append(index)
+
+            time.sleep(1)
+
+        else:
+
+            article_list += craw_page(res, push_rate)
+
+        time.sleep(0.05)
+
+
+
+    total = len(article_list)
+
+    count = 0
+
+    image_seq = []
+
+    # 進入每篇文章分析內容
+
+    while article_list:
+
+        article = article_list.pop(0)
+
+        res = rs.get(article['url'], verify=False)
+
+        # 如網頁忙線中,則先將網頁加入 index_list 並休息1秒後再連接
+
+        if res.status_code != 200:
+
+            article_list.append(article)
+
+            time.sleep(1)
+
+        else:
+
+            count += 1
+
+            image_seq += download_beauty.store_pic(article['url'])
+
+            write_db(image_seq, session)
+
+            print('download: {:.2%}'.format(count / total))
+
+        time.sleep(0.05)
+
+
+
+    # disconnect
+
+    session.close()
+
+    engine.dispose()
+
+
+
+    print("下載完畢...")
+
+    print('execution time: {:.3}s'.format(time.time() - start_time))
+
+
+
+
+
+if __name__ == '__main__':
+
+    print('main')
+
+    main()
+
+    schedule.every(30).minutes.do(main)
+
+    while True:
+
+        print('wating......')
+
+        schedule.run_pending()
+
+        time.sleep(1)
 
 def crawl_page_gossiping(res):
     soup = BeautifulSoup(res.text, 'html.parser')
