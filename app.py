5769e4cb7ecb4503476ba03ce8316a2129c95875
kkiiddss <34616443+kkiiddss@users.noreply.github.com>
2017-12-18 16:55:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ from linebot.models import *
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('')
-handler = WebhookHandler('')
+line_bot_api = LineBotApi('9UqcHjiCzjEpbxBa//hlWWFvY79FC2PVc9CfRlhaOOXSozfPWpfsK4rSFpF/lwNwgsoS7qnKrk4TKTZes9CFdTFxAAUztx6VFZ8o3mPUlvJkVHrE2dsVoaN+UOSIU7A58/zbdp5TtFQpJWWGuSnyRAdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('c9e4610cf78163336e6cc81319358d5d')
 
 
 @app.route("/callback", methods=['POST'])
@@ -33,69 +33,6 @@ def callback():
         abort(400)
 
     return 'OK'
-
-
-def apple_news():
-    target_url = 'https://tw.appledaily.com/new/realtime'
-    res = requests.get(target_url)
-    soup = BeautifulSoup(res.text, 'html.parser')
-    content = ""
-
-    for index, data in enumerate(soup.select('.rtddt a'), 0):
-        if index == 10:
-            return content
-
-        heading = data.select_one('h1').text
-        link = data['href']
-
-        content += "{}\n{}\n\n".format(heading, link)
-
-
-def movie_thisweek():
-    target_url = 'https://tw.movies.yahoo.com/movie_thisweek.html'
-    res = requests.get(target_url)
-    soup = BeautifulSoup(res.text, 'html.parser')
-    pages = len(soup.select_one('.page_numbox').find_all('li')) - 4
-    content = ""
-
-    for i in range(pages):
-        if i == 0:
-            pass
-        else:
-            target_url = 'https://tw.movies.yahoo.com/movie_thisweek.html?page={}'.format(str(i + 1))
-            res = requests.get(target_url)
-            soup = BeautifulSoup(res.text, 'html.parser')
-
-        for data in soup.select('.release_info_text'):
-            heading = data.find('div', attrs={'class': 'release_movie_name'}).find('a', attrs={'class': 'gabtn'}, href=True)
-            name = heading.text.strip()
-            link = heading['href']
-            time = data.find('div', attrs={'class': 'release_movie_time'}).text.split('：')[1].strip()
-            expectancy = data.find('div', attrs={'class': 'leveltext'}).select_one('span').text
-
-            content += "{}\n{}\n期待度: {}\n{}\n\n".format(name, time, expectancy, link)
-
-    return content
-
-
-def movie_intheaters():
-    target_url = 'https://tw.movies.yahoo.com/movie_intheaters.html'
-    req = requests.get(target_url)
-    res = BeautifulSoup(req.text, 'html.parser')
-    content = ""
-
-    for data in res.select('.release_info_text'):
-        heading = data.find('div', attrs={'class': 'release_movie_name'}).find('a', attrs={'class': 'gabtn'}, href=True)
-        name = heading.text.strip()
-        link = heading['href']
-        time = data.find('div', attrs={'class': 'release_movie_time'}).text.split('：')[1].strip()
-        expectancy = data.find('div', attrs={'class': 'leveltext'}).select_one('span').text
-        rate = data.find('div', attrs={'class': 'starwithnum'}).select_one('span')['data-num']
-
-        content += "{}\n{}\n期待度: {}\n滿意度: {} 星\n{}\n\n".format(name, time, expectancy, rate, link)
-
-    return content
-
 
 def crawl_ptt(res, board, session=None):
     soup = BeautifulSoup(res.text, 'html.parser')
@@ -139,17 +76,6 @@ def crawl_ptt(res, board, session=None):
     return content
 
 
-def ptt_gossiping():
-    rs = requests.session()
-    data = {
-        'from': '/bbs/Gossiping/index.html',
-        'yes': 'yes'
-    }
-
-    res = rs.post('https://www.ptt.cc/ask/over18', verify=False, data=data)
-    content = crawl_ptt(res, 'Gossiping', rs)
-
-    return "\n".join(content)
 
 
 def ptt_beauty():
@@ -195,17 +121,15 @@ def ptt_random_pic():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if event.message.text == 'Apple news':
-        content = apple_news()
-    elif event.message.text == 'Movie thisweek':
-        content = movie_thisweek()
-    elif event.message.text == 'Movie intheaters':
-        content = movie_intheaters()
-    elif event.message.text == 'PTT Gossiping':
-        content = ptt_gossiping()
-    elif event.message.text == 'PTT Beauty':
-        content = ptt_beauty()
-    elif event.message.text == '插':
+    if event.message.text == '吹':
+        content = ptt_random_pic()
+    elif event.message.text =='含':
+        content = ptt_random_pic()
+    elif event.message.text == '吸':
+        content = ptt_random_pic()
+    elif event.message.text == '舔':
+        content = ptt_random_pic()
+    elif event.message.text == '扣':
         content = ptt_random_pic()
         image_message = ImageSendMessage(
             original_content_url=content,
@@ -218,99 +142,7 @@ def handle_message(event):
         )
 
         return
-    elif event.message.text == 'Yahoo movies':
-        buttons_template = TemplateSendMessage(
-            alt_text='目錄 template',
-            template=ButtonsTemplate(
-                title='選擇服務',
-                text='請選擇',
-                actions=[
-                    MessageTemplateAction(
-                        label='本週新片',
-                        text='Movie thisweek'
-                    ),
-                    MessageTemplateAction(
-                        label='上映中',
-                        text='Movie intheaters'
-                    )
-                ]
-            )
-        )
-
-        line_bot_api.reply_message(
-            event.reply_token,
-            buttons_template
-        )
-
-        return
-    elif event.message.text == 'PTT':
-        buttons_template = TemplateSendMessage(
-            alt_text='目錄 template',
-            template=ButtonsTemplate(
-                title='選擇服務',
-                text='請選擇',
-                actions=[
-                    MessageTemplateAction(
-                        label='PTT八卦板最新文章',
-                        text='PTT Gossiping'
-                    ),
-                    MessageTemplateAction(
-                        label='PTT表特板大於10推文章',
-                        text='PTT Beauty'
-                    ),
-                    MessageTemplateAction(
-                        label='隨機一張表特正妹圖片',
-                        text='PTT random picture'
-                    )
-                ]
-            )
-        )
-
-        line_bot_api.reply_message(
-            event.reply_token,
-            buttons_template
-        )
-
-        return
-    else:
-        buttons_template = TemplateSendMessage(
-            alt_text='目錄 template',
-            template=ButtonsTemplate(
-                title='選擇服務',
-                text='請選擇',
-                actions=[
-                    MessageTemplateAction(
-                        label='即時新聞',
-                        text='Apple news'
-                    ),
-                    MessageTemplateAction(
-                        label='Yahoo奇摩電影',
-                        text='Yahoo movies'
-                    ),
-                    MessageTemplateAction(
-                        label='PTT',
-                        text='PTT'
-                    ),
-                    URITemplateAction(
-                        label='查看原始碼',
-                        uri='https://github.com/damnee562/Damien-bot'
-                    )
-                ]
-            )
-        )
-
-        line_bot_api.reply_message(
-            event.reply_token,
-            buttons_template
-        )
-
-        return
-
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=content)
-    )
-
-
+  
+    
 if __name__ == "__main__":
     app.run()
