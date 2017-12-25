9973cfd844db1431bb13686c3700da780090a602
kkiiddss <34616443+kkiiddss@users.noreply.github.com>
2017-12-25 13:49:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -118,8 +118,7 @@ def handle_message(event):
             original_content_url=content,
             preview_image_url=content
         )
-        )
-
+       
         line_bot_api.reply_message(
             event.reply_token,
             image_message
